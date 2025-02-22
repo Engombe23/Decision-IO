df3e750b2612a5f48e45cf8f21a8649edcf7dab6
nikhilp1234567 <nikhil_parmar@hotmail.com>
2025-02-22 14:48:13 +0000

    F add a step state and form

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,22 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "../component-css/Home.css";
+import axios from "axios";
 
 export default function Home() {
-  function handleSubmit(e: React.FormEvent) {
+  const [category, setCategory] = useState("");
+  const [transition, setTransition] = useState(false);
+  const [step, setStep] = useState(0);
+
+  async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
+    console.log(category);
+    setStep(step + 1);
+    // const data = await axios.post("...");
+    setTransition(true);
   }
+
+  const renderStepContent = () => {
+    switch (step) {
+      case 0:
+        return (
+          <form className='form' action=''>
+            <input
+              className='select-input'
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              type='text'
+              name='category'
+              placeholder='Input a category (eg. Picking a Resteraunt, Choosing a TV show)'
+            />
+            <button onClick={handleSubmit} className='submit-button'>
+              Let's Decide...
+            </button>
+          </form>
+        );
+      case 1:
+        return <div>Step 2 Content</div>;
+      case 2:
+        return <div>Step 3 Content</div>;
+      default:
+        return <div>Invalid Step</div>;
+    }
+  };
+
   return (
     <div className='home-container'>
       <div id='left' className='left-section'>
-        <div id='form'>
-          <div id='title' className='title'></div>
-          <div id='subtitile' className='description'></div>
-          <div>
-            <form className='form' action=''>
-              <input type='dropdown' className='select-input' />
-              <button onClick={handleSubmit} className='submit-button'></button>
-            </form>
+        <div id='holder' className='holder'>
+          <div id='title' className='title'>
+            DECISION.IO
           </div>
+          <div id='subtitile' className='description'>
+            A smart tool that harnesses AI and probability theory to mathematically help you make better decisions.
+          </div>
+          <div>{renderStepContent()}</div>
         </div>
       </div>
       <div id='right' className='right-section'></div>
