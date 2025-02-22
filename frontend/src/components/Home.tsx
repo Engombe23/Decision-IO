9c108919ba9af0c6453d5356f34dd06cf5d27eff
nikhilp1234567 <nikhil_parmar@hotmail.com>
2025-02-22 13:49:33 +0000

    do styling with normal css

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import "../component-css/Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+  }
+  return (
+    <div className='home-container'>
+      <div id='left' className='left-section'>
+        <div id='form'>
+          <div id='title' className='title'></div>
+          <div id='subtitile' className='description'></div>
+          <div>
+            <form className='form' action=''>
+              <input type='dropdown' className='select-input' />
+              <button onClick={handleSubmit} className='submit-button'></button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div id='right' className='right-section'></div>
+    </div>
+  );
 }
